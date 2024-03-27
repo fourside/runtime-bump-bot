5945b06ed5fc8969f693802a42eea464e7d84b63
fourside <fourside@gmail.com>
2024-03-27 11:14:02 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import {
   fetchNodeCycles,
 } from "./fetch-cycles";
 import { getContents, getSha, searchContents } from "./github";
+import {
+  Dockerfile,
+  GitHubActions,
+  Nvm,
+  type TargetFileType,
+} from "./target-file";
 
 async function main(): Promise<void> {
   const [latestNode, latestDebian] = await Promise.all([
@@ -19,126 +25,25 @@ async function main(): Promise<void> {
 
   const baseSha = await getSha({ owner, repo, branch: baseBranch });
 
-  const nodeContents: NodeContent[] = [];
-
-  for (const path of Object.keys(nodeUpdater)) {
-    const contents = await getContents({ owner, repo, path, ref: baseSha });
-    for (const content of contents) {
-      nodeContents.push({
-        key: path,
-        contentPath: content.path,
-        content: Buffer.from(content.content, "base64").toString("utf-8"),
-      });
+  const targetFileContentsList = await Promise.all(
+    [Nvm, GitHubActions, Dockerfile].map((it) =>
+      getTargetFileContents(it, owner, repo, baseSha),
+    ),
+  );
+  for (const content of targetFileContentsList.flat()) {
+    const getVersions =
+      content.type === "nvm"
+        ? Nvm.getNodeVersions
+        : content.type === "GitHub Actions"
+          ? GitHubActions.getNodeVersions
+          : Dockerfile.getNodeVersions;
+    const versions = getVersions(content.content);
+    if (versions.length === 0) {
+      continue;
     }
-  }
-
-  for (const content of nodeContents) {
-    const { getVersion, replace } = nodeUpdater[content.key];
-    const version = getVersion(content.content);
-    console.log({ contentPath: content.contentPath, version });
-  }
-
-  const dockerfiles = await searchContents({
-    owner,
-    repo,
-    ref: baseSha,
-    filename: "Dockerfile",
-  });
-  const dockerfileContents: DockerfileContent[] = [];
-  for (const dockerfile of dockerfiles) {
-    dockerfileContents.push({
-      path: dockerfile.path,
-      content: Buffer.from(dockerfile.content, "base64").toString("utf-8"),
-    });
-  }
-  for (const content of dockerfileContents) {
-    const {
-      getNodeVersions: getNodeVersion,
-      replaceNodeVersion,
-      getDevianVersions: getDevianVersion,
-      replaceDebianVersion,
-    } = dockerfileUpdater;
-    const nodeVersion = getNodeVersion(content.content);
-    const debianVersion = getDevianVersion(content.content);
-    console.log({ nodeVersion, debianVersion });
+    console.log({ path: content.path, versions });
   }
 }
-
-type PathString = string;
-type NodeUpdater = Record<
-  PathString,
-  {
-    getVersion: (content: string) => string | undefined;
-    replace: (content: string, newVersion: string) => string;
-  }
->;
-
-type NodeContent = {
-  key: keyof NodeUpdater;
-  contentPath: string;
-  content: string;
-};
-
-const nodeUpdater: NodeUpdater = {
-  ".github/workflows": {
-    getVersion: (content) => {
-      const result = [...content.matchAll(/node-version: "(.+).x"/g)];
-      if (result.length === 0) {
-        return undefined;
-      }
-      return result[0][1];
-    },
-    replace: (content, newVersion) => {
-      return content.replace(
-        /node-version: ".+"/g,
-        `node-version: "${newVersion}.x"`,
-      );
-    },
-  },
-  ".nvmrc": {
-    getVersion: (content) => {
-      return content.trim();
-    },
-    replace: (content, newVersion) => {
-      return content.replace(/^.+$/, newVersion);
-    },
-  },
-};
-
-type DockerfileUpdater = {
-  getNodeVersions: (content: string) => string[];
-  replaceNodeVersion: (content: string, newVersion: string) => string;
-  getDevianVersions: (content: string) => string[];
-  replaceDebianVersion: (content: string, newVersion: string) => string;
-};
-
-type DockerfileContent = {
-  path: string;
-  content: string;
-};
-
-const dockerfileUpdater: DockerfileUpdater = {
-  getNodeVersions: (content) => {
-    const result = [...content.matchAll(/FROM node:(.+?)-/gi)];
-    if (result.length === 0) {
-      return [];
-    }
-    return result.map((it) => it[1]);
-  },
-  replaceNodeVersion: (content, newVersion) => {
-    return content.replace(/(?<=FROM node:).+?-/g, `${newVersion}-`);
-  },
-  getDevianVersions: (content) => {
-    const result = [...content.matchAll(/FROM .+?-(.+?)[- ]/gi)];
-    if (result.length === 0) {
-      return [];
-    }
-    return result.map((it) => it[1]);
-  },
-  replaceDebianVersion: (content, newVersion) => {
-    return content.replace(/(?<=FROM .+?-).+?-/gi, `${newVersion}-`);
-  },
-};
 
 async function fetchLatestNodeLTS(): Promise<NodeCycle> {
   const cycles = await fetchNodeCycles();
@@ -161,6 +66,52 @@ async function fetchLatestDebian(): Promise<DebianCycle> {
     })
     .sort((a, b) => Number.parseInt(b.cycle) - Number.parseInt(a.cycle));
   return sorted[0];
+}
+
+type TargetFileContent = {
+  type: TargetFileType["type"];
+  path: string;
+  content: string;
+};
+
+async function getTargetFileContents(
+  targetFile: TargetFileType,
+  owner: string,
+  repo: string,
+  ref: string,
+): Promise<TargetFileContent[]> {
+  if (targetFile.type === "nvm" || targetFile.type === "GitHub Actions") {
+    const contents = await getContents({
+      owner,
+      repo,
+      path: targetFile.path,
+      ref,
+    });
+    return contents.map((it) => {
+      return {
+        type: targetFile.type,
+        path: it.path,
+        content: Buffer.from(it.content, "base64").toString("utf-8"),
+      };
+    });
+  }
+  if (targetFile.type === "Docker") {
+    const contents = await searchContents({
+      owner,
+      repo,
+      filename: Dockerfile.path,
+      ref,
+    });
+    return contents.map((it) => {
+      return {
+        type: targetFile.type,
+        path: it.path,
+        content: Buffer.from(it.content, "base64").toString("utf-8"),
+      };
+    });
+  }
+  // TODO: exhaustive
+  return [];
 }
 
 (async () => {
---
+++ src/target-file.ts
@@ -0,0 +1,78 @@
+type NvmType = {
+  type: "nvm";
+  path: ".nvmrc";
+  getNodeVersions: (content: string) => string[];
+  updateNode: (content: string, newVersion: string) => string;
+};
+
+type GitHubActionsType = {
+  type: "GitHub Actions";
+  path: ".github/workflows";
+  getNodeVersions: (content: string) => string[];
+  updateNode: (content: string, newVersion: string) => string;
+};
+
+type DockerType = {
+  type: "Docker";
+  path: "Dockerfile";
+  getNodeVersions: (content: string) => string[];
+  getDebianVersions: (content: string) => string[];
+  updateNode: (content: string, newVersion: string) => string;
+  updateDebian: (content: string, newVersion: string) => string;
+};
+
+export type TargetFileType = NvmType | GitHubActionsType | DockerType;
+
+export const Nvm: NvmType = {
+  type: "nvm",
+  path: ".nvmrc",
+  getNodeVersions: (content) => {
+    return [content.trim()];
+  },
+  updateNode: (content, newVersion) => {
+    return content.replace(/^.+$/, newVersion);
+  },
+};
+
+export const GitHubActions: GitHubActionsType = {
+  type: "GitHub Actions",
+  path: ".github/workflows",
+  getNodeVersions: (content) => {
+    const result = [...content.matchAll(/node-version: "(.+).x"/g)];
+    if (result.length === 0) {
+      return [];
+    }
+    return result.map((it) => it[1]);
+  },
+  updateNode: (content, newVersion) => {
+    return content.replace(
+      /node-version: ".+"/g,
+      `node-version: "${newVersion}.x"`,
+    );
+  },
+};
+
+export const Dockerfile: DockerType = {
+  type: "Docker",
+  path: "Dockerfile",
+  getNodeVersions: (content) => {
+    const result = [...content.matchAll(/FROM node:(.+?)-/gi)];
+    if (result.length === 0) {
+      return [];
+    }
+    return result.map((it) => it[1]);
+  },
+  updateNode: (content, newVersion) => {
+    return content.replace(/(?<=FROM node:).+?-/g, `${newVersion}-`);
+  },
+  getDebianVersions: (content) => {
+    const result = [...content.matchAll(/FROM .+?-(.+?)[- ]/gi)];
+    if (result.length === 0) {
+      return [];
+    }
+    return result.map((it) => it[1]);
+  },
+  updateDebian: (content, newVersion) => {
+    return content.replace(/(?<=FROM .+?-).+?-/gi, `${newVersion}-`);
+  },
+};
